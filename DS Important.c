b7d35877faedf09364a0d7a53e8c56743172a4c9
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-15 10:37:05 +0530

    Add files via upload

---
+++ DS Important.c
@@ -0,0 +1,140 @@
+
+
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node*next;
+};
+void create_node(struct node**head,int d);
+void create_node(struct node**head,int d){
+	struct node*temp=malloc(sizeof(struct node*));
+	temp->data=d;
+	temp->next=NULL;
+	*head=temp;
+}
+void display(struct node*head);
+void display(struct node*head)
+{
+	struct node*ptr=head;
+	while(ptr!=NULL)
+	{
+		printf("%d ",ptr->data);
+		ptr=ptr->next;
+	}
+}
+int count_of_nodes(struct node*head);
+int count_of_nodes(struct node*head)
+{
+	int count=0;
+struct node*ptr=head;
+	while(ptr!=NULL)
+	{
+		count++;
+		ptr=ptr->next;
+	}
+	printf("number of nodes = %d",count);	
+}
+void insert_at_beginning(struct node**head,int d);
+void insert_at_beginning(struct node**head,int d)
+{
+	struct node*temp=malloc(sizeof(struct node*));
+	temp->data=d;
+	temp->next=NULL;
+	temp->next=*head;
+	*head=temp;
+}
+void insert_at_end(struct node*head,int d);
+void insert_at_end(struct node*head,int d)
+{
+struct node*temp=malloc(sizeof(struct node*));
+	temp->data=d;
+	temp->next=NULL;
+	struct node*ptr=head;
+	while(ptr->next!=NULL)
+	{
+		ptr=ptr->next;
+	}
+	ptr->next=temp;
+		
+}
+void insert_at_position(struct node*head,int d,int pos);
+void insert_at_position(struct node*head,int d,int pos)
+{
+	struct node*temp=malloc(sizeof(struct node*));
+	temp->data=d;
+	temp->next=NULL;
+	struct node*ptr=head;
+	pos--;
+	while(pos!=1)
+	{
+		ptr=ptr->next;
+		pos--;
+	}
+	temp->next=ptr->next;
+	ptr->next=temp;
+}
+void delete_at_beginning(struct node**head);
+void delete_at_beginning(struct node**head)
+{
+struct node *ptr=*head;
+(*head)=(*head)->next;
+free(ptr);
+
+}
+void delete_at_end(struct node*head);
+void delete_at_end(struct node*head){
+struct node*current =malloc(sizeof(struct node *));
+struct node*ptr=head;
+while(ptr->next!=NULL)
+	{
+    current=ptr;
+		ptr=ptr->next;
+	}
+    current->next=NULL;
+    free(ptr);
+    
+
+}
+void delete_at_position(struct node*head,int pos);
+void delete_at_position(struct node*head,int pos)
+{
+struct node*ptr=head;
+struct node*ptr1;
+pos--;
+while(pos!=1)
+{
+ptr=ptr->next;
+pos--;
+}
+ptr1->next=ptr->next;
+free(ptr);
+}
+int main()
+{
+	struct node*head=NULL;
+	head=(struct node*)malloc(sizeof(struct node*));
+	create_node(&head,10);
+	display(head);
+	count_of_nodes(head);
+    printf("\n");
+	insert_at_beginning(&head,40);
+	display(head);
+    printf("\n");
+	insert_at_end(head,30);
+	display(head);
+    printf("\n");
+	insert_at_position(head,60,2);
+	display(head);
+    printf("\n");
+	delete_at_beginning(&head);
+	display(head);
+    printf("\n");
+    delete_at_end(head);
+    display(head);
+    printf("\n");
+    delete_at_position(head,2);
+    display(head);
+    
+	
+}
