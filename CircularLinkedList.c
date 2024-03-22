d2a2c5549a8d1a90a4a8adf09638233e93b57cd6
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-22 13:53:13 +0530

    Update CircularLinkedList.c

--- CircularLinkedList.c
+++ CircularLinkedList.c
@@ -1,78 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
 struct node
+  {
+   int data;
+   struct node*next;
+  };
+  void create_node(struct node**tail,int d);
+  void create_node(struct node**tail,int d)
+  {
+    struct node*temp=malloc(sizeof(struct node*));
+     temp->data=d;
+     temp->next=temp;
+     (*tail)=temp;
+  }
+  
+  void display(struct node*tail);
+  void display(struct node*tail)
+    {
+        struct node*ptr=tail->next;
+          do
+    { 
+       printf("%d ",ptr->data);
+       ptr=ptr->next; 
+    }
+     while(ptr!=tail->next);
+
+  }
+  
+  
+  void insert_at_beginning(struct node*tail,int d);
+  void insert_at_beginning(struct node*tail,int d)
+    {
+      struct node*temp=malloc(sizeof(struct node*));
+        temp->data=d;
+        temp->next=NULL;
+        temp->next=tail->next;
+        tail->next=temp;
+    } 
+    
+    
+   void insert_at_end(struct node**tail,int d);
+   void insert_at_end(struct node**tail,int d)
+    {
+     struct node*temp=malloc(sizeof(struct node*));
+      temp->data=d;
+      temp->next=NULL; 
+      temp->next=(*tail)->next;
+       (*tail)->next=temp;
+       (*tail)=temp;
+    }
+    
+    
+   void insert_at_position(struct node**tail,int d,int pos);
+   void insert_at_position(struct node**tail,int d,int pos)
 {
-int data;
-struct node*next;
-};
-void create_node(struct node**tail,int d);
-void create_node(struct node**tail,int d)
-{
-struct node*temp=malloc(sizeof(struct node*));
-temp->data=d;
-temp->next=temp;
-*tail=temp;
+      struct node*temp=malloc(sizeof(struct node*));
+        temp->data=d;
+        temp->next=NULL;
+      struct node*ptr=(*tail)->next;
+         pos--;
+      while(pos>1)
+   {
+       ptr=ptr->next;
+       pos--;
+   }
+      temp->next=ptr->next;
+      ptr->next=temp;
 }
-void display(struct node*tail);
-void display(struct node*tail)
-{
-struct node*ptr=tail->next;
-do
-{
-printf("%d ",ptr->data);
-ptr=ptr->next;
-}
-while(ptr!=tail->next);
-}
-void insert_at_beginning(struct node*tail,int d);
-void insert_at_beginning(struct node*tail,int d)
-{
-struct node*temp=malloc(sizeof(struct node*));
-temp->data=d;
-temp->next=NULL;
-temp->next=tail->next;
-tail->next=temp;
-}
-void insert_at_end(struct node**tail,int d);
-void insert_at_end(struct node**tail,int d)
-{
- struct node*temp=malloc(sizeof(struct node*));
-  temp->data=d;
-  temp->next=NULL; 
-  temp->next=(*tail)->next;
-  (*tail)->next=temp;
-  (*tail)=temp;
-}
-void insert_at_position(struct node**tail,int d,int pos);
-void insert_at_position(struct node**tail,int d,int pos)
-{
-struct node*temp=malloc(sizeof(struct node*));
-temp->data=d;
-temp->next=NULL;
-struct node*ptr=(*tail)->next;
-pos--;
-while(pos>1)
- {
-   ptr=ptr->next;
-   pos--;
- }
-temp->next=ptr->next;
-ptr->next=temp;
 
 
-}
-void delete_at_beginning(struct node*tail);
-void delete_at_beginning(struct node*tail)
+  void delete_at_beginning(struct node*tail);
+  void delete_at_beginning(struct node*tail)
 {
-struct node*ptr=tail->next;
-tail->next=ptr->next;
-free(ptr);
-ptr=NULL;
-if(tail->next==tail)
- {
-   free(ptr);
-   tail=NULL;
- }
+       struct node*ptr=tail->next;
+         tail->next=ptr->next;
+          free(ptr);
+         ptr=NULL;
+       if(tail->next==tail)
+        { 
+         free(ptr);
+         tail=NULL;
+        }
 }
 
 int main()
@@ -94,4 +102,3 @@ int main()
      display(tail);
    printf("\n");
 }
-
