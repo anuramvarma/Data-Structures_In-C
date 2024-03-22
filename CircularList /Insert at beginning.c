296f3cf909f324de1be7b8487c199b9068014fa3
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-22 14:29:55 +0530

    Create Insert at beginning.c

---
+++ CircularList /Insert at beginning.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+    {
+      int data;
+      struct node*next;
+    };
+   void create_node(struct node**tail,int d);
+   void create_node(struct node**tail,int d)
+ {
+    struct node*temp=malloc(sizeof(struct node*));
+     temp->data=d;
+     temp->next=temp;
+     (*tail)=temp;
+ }
+  
+  void display(struct node*tail);
+  void display(struct node*tail)
+    {
+        struct node*ptr=tail->next;
+          do
+      { 
+       printf("%d ",ptr->data);
+       ptr=ptr->next; 
+      }
+     while(ptr!=tail->next);
+    }
+  
+  void insert_at_beginning(struct node*tail,int d);
+  void insert_at_beginning(struct node*tail,int d)
+    { 
+      struct node*temp=malloc(sizeof(struct node*));
+        temp->data=d;
+        temp->next=NULL;
+        temp->next=tail->next;
+        tail->next=temp;
+    } 
+    
+ 
+int main()
+{
+   struct node*tail=NULL;
+      create_node(&tail,10);
+      display(tail);
+   printf("\nInsert at beginning :\n");
+     insert_at_beginning(tail,20);
+     display(tail);
+   printf("\n");
+}
