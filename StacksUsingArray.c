b0b4e89bb6ad1bbba13f709d7f644dd5de723738
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-04-12 14:43:10 +0530

    Update StacksUsingArray.c

--- StacksUsingArray.c
+++ StacksUsingArray.c
@@ -18,29 +18,29 @@ int main()
         switch(choice)
         {
             case 1:
-            {
+              {
                 push();
                 break;
-            }
-            case 2:
-            {
+              }
+           case 2:
+             {
                 pop();
                 break;
-            }
+             }
             case 3:
-            {
+             {
                 display();
                 break;
-            }
+             }
             case 4:
-            {
+             {
                 printf("\n\t EXIT POINT ");
                 break;
-            }
+             }
             default:
-            {
+             {
                 printf ("\n\t Please Enter a Valid Choice(1/2/3/4)");
-            }
+             }
                 
         }
     }
@@ -50,42 +50,42 @@ int main()
 void push()
 {
     if(top>=n-1)
-    {
+     {
         printf("\n\tSTACK is over flow");
         
-    }
+     }
     else
-    {
+      {
         printf(" Enter a value to be pushed:");
         scanf("%d",&x);
         top++;
         stack[top]=x;
-    }
+      }
 }
 void pop()
 {
     if(top<=-1)
-    {
-        printf("\n\t Stack is under flow");
-    }
+     {
+      printf("\n\t Stack is under flow");
+     }
     else
-    {
+      {
         printf("\n\t The popped elements is %d",stack[top]);
         top--;
-    }
+      }
 }
 void display()
 {
     if(top>=0)
-    {
+      {
         printf("\n The elements in STACK \n");
         for(i=top; i>=0; i--)
             printf("\n%d",stack[i]);
         printf("\n Press Next Choice");
-    }
+      }
     else
-    {
+      {
         printf("\n The STACK is empty");
-    }
+      }
    
 }
