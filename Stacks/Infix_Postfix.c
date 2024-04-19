a644ae8e17b6244177b29276d67c74fcd50687cf
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-04-19 14:43:15 +0530

    Update Infix_Postfix.c

--- Stacks/Infix_Postfix.c
+++ Stacks/Infix_Postfix.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-#define max 50               
+#define max 100            
 char stack[max];
 int top = -1;
-char infix[50],postfix[50];
+char infix[100],postfix[100];
 void push(char element)     
-{
+      {
 	stack[++top]=element;
-}
+      }
 char pop()
-{
-	return(stack[top--]);
-}
+    {
+      return(stack[top--]);
+     }
 int priority(char x)
 {
     if(x == '(')
@@ -24,41 +24,42 @@ int priority(char x)
     return 0;   
 } 
  int check(char ch)   
-{   
+  {   
     return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')|| (ch>='0' && ch<='9' ));   
-}   
+  }   
 int main(void) 
 {
 	char ch,element;
 	int i=0,k=0;
-	printf("READ THE INFIX\n\n");
-	scanf("%s",infix);
+	printf("ENTER THE INFIX\n\n");
+	 scanf("%s",infix);
 	push('#');                                 
-    
 	while((ch = infix[i++])!='\0')
   {
 		if(ch=='(')
-		push(ch);
+		  push(ch);
 		else if(check(ch))
-		postfix[k++]=ch;
-		else if(ch==')'){
+		  postfix[k++]=ch;
+		else if(ch==')')
+		{
 			while(stack[top]!='(')
 			{
-			postfix[k++]=pop();
+			 postfix[k++]=pop();
 			}
 			element=pop();                    
 		}
-		else{
-			while(priority(stack[top])>=priority(ch))  
-			postfix[k++]=pop();
-			push(ch);
-		}
+		else
+		 {
+		    while(priority(stack[top])>=priority(ch))  
+		      postfix[k++]=pop();
+		       push(ch);
+		 }
 	}
 	while(stack[top]!='#')
-  {
-		postfix[k++]=pop();
-	}
-	postfix[k]='\0';
-	printf("\n\nGiven Infix Expn: %s\nPostfix Expn: %s\n", infix, postfix);
+        {   
+         postfix[k++]=pop();
+        }
+	  postfix[k]='\0';
+	printf("\n\nGiven Infix Expression: %s\nPostfix Expression: %s\n", infix, postfix);
 	return 0;
 }
