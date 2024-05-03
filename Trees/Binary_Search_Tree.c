5c0eeafa0dce508e3252aa91cb8ab547d622a39f
AnuramVarma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-05-03 16:00:50 +0530

    Update Binary_Search_Tree.c

--- Trees/Binary_Search_Tree.c
+++ Trees/Binary_Search_Tree.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node {
+struct node 
+{
 	int data;
 	struct node *left;
 	struct node *right;
@@ -18,62 +19,52 @@ struct node * insert(struct node *,int );
 struct node * insert(struct node *root,int d)
 {
 	if(root==NULL)
-	{
-		return create_node(d);
-	}
+	      {
+	      	return create_node(d);
+       	}
 	if(d<root->data)
-	{
+	      {
 		root->left=insert(root->left,d);
-	}
+	      }
 	if(d>root->data)
-	{
+	    {
 		root->right=insert(root->right,d);
-	}
+	    }
 	return root;
 }
 void Inorder(struct node *);
 void Inorder(struct node *root)
 {
-	if(root->left!=NULL)
-	
+	if(root->left!=NULL)	
 	Inorder(root->left);
 	
 	printf("%d ",root->data);
 	if(root->right!=NULL)
 	{
 	Inorder(root->right);
-	}
-		
-	
+	}			
 }
 void preorder(struct node *);
 void preorder(struct node *root)
 {
 	printf("%d ",root->data);
-	if(root->left!=NULL)
-	
-	Inorder(root->left);
-	
-	
-	if(root->right!=NULL)
-	{
-	Inorder(root->right);
-	}
-		
-	
-}
-void postorder(struct node *);
-void postorder(struct node *root)
-{
-	if(root->left!=NULL)
-	
+	if(root->left!=NULL)	
 	Inorder(root->left);	
 	if(root->right!=NULL)
 	{
 	Inorder(root->right);
+	}			
+}
+void postorder(struct node *);
+void postorder(struct node *root)
+{
+	if(root->left!=NULL)	
+	Inorder(root->left);		
+	if(root->right!=NULL)
+	{
+	Inorder(root->right);
 	}
-	printf("%d ",root->data);
-		
+	printf("%d ",root->data);		
 }
 int main()
 {
@@ -81,12 +72,32 @@ int main()
 	root=insert(root,50);
 	root=insert(root,20);
 	root=insert(root,30);
-	
-	root=insert(root,60);
-	Inorder(root);
-	printf("\npostorder\n");
-	postorder(root);
-	printf("\npreorder\n");
-	preorder(root);
-	
+	root=insert(root,60);		
+	while (1)
+	{
+	   int choice;
+ 	printf("\n1.Inorder");
+	printf("\n2.Postorder");
+	printf("\n3.Preorder");
+	printf("\n4.Exit");
+	printf("\nEnter an option to perform a task :");
+          scanf("%d",&choice);
+       	switch (choice)
+	{
+	case 1 : 
+		printf("Inorder : ");
+		Inorder(root);
+		break;
+	case 2: 
+		printf("Postorder : ");
+		postorder(root);
+		break;
+	case 3 : 
+		printf("Preorder : ");
+		preorder(root);
+		break;
+	case 4 : exit(1);	
+	default: printf("Enter a Choice from 1-4 ONLY");
+	}
+	}
 }
